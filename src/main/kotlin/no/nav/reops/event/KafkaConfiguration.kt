36feab8c85849402d63f4cec7f015e265d593be4
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-04-08 06:37:06 +0200

    feat: rename topic

--- src/main/kotlin/no/nav/reops/event/KafkaConfiguration.kt
+++ src/main/kotlin/no/nav/reops/event/KafkaConfiguration.kt
@@ -1,9 +1,12 @@
 package no.nav.reops.event
 
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.kafka.autoconfigure.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.TopicBuilder
 import org.springframework.kafka.core.DefaultKafkaProducerFactory
 import org.springframework.kafka.core.KafkaTemplate
 
@@ -13,8 +16,22 @@ class KafkaConfiguration(
     private val kafkaProperties: KafkaProperties
 ) {
 
-    @Bean
+    @Bean("defaultRetryTopicKafkaTemplate")
     fun kafkaTemplate(): KafkaTemplate<String, Event> {
         return KafkaTemplate(DefaultKafkaProducerFactory(kafkaProperties.buildProducerProperties()))
+    }
+
+    @Bean
+    fun retryTopic(@Value("\${spring.kafka.topic}") mainTopic: String): NewTopic {
+        return TopicBuilder.name("$mainTopic-retry")
+            .partitions(1)
+            .build()
+    }
+
+    @Bean
+    fun dltTopic(@Value("\${spring.kafka.topic}") mainTopic: String): NewTopic {
+        return TopicBuilder.name("$mainTopic-dlt")
+            .partitions(1)
+            .build()
     }
 }
