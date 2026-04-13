e66202cf0403810f5a5485eeb7549fcf1d7b41a1
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-04-13 21:26:22 +0200

    feat: tracing

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+	implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
